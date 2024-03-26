79e16bcb9808e28538b8d9d97ea2b176a6cfdf5e
jsnb2333 <1357930097@qq.com>
2024-03-26 17:28:10 +0800

    feat:vue-project

---
+++ vue-project/src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref, watch } from 'vue';
+const count = ref(0);
+const count2 = ref(0);
+
+function countAdd() {
+	count.value++;
+	count2.value++;
+}
+
+debugger
+watch(() => {
+	return count.value + count2.value
+}, () => {
+	console.log('执行了');
+},{flush:'sync'});
+</script>
+
+<template>
+	<div>
+		<button @click="countAdd">
+			{{ count }}
+		</button>
+	</div>
+</template>
+
+<style scoped>
+header {
+	line-height: 1.5;
+}
+
+.logo {
+	display: block;
+	margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+	header {
+		display: flex;
+		place-items: center;
+		padding-right: calc(var(--section-gap) / 2);
+	}
+
+	.logo {
+		margin: 0 2rem 0 0;
+	}
+
+	header .wrapper {
+		display: flex;
+		place-items: flex-start;
+		flex-wrap: wrap;
+	}
+}
+</style>
---
+++ vue-project/src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+debugger
+createApp(App).mount('#app')
